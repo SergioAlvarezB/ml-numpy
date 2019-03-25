add KNN regressor class and add example in vanilla-knn notebook, fix gradient computation in SVM

models/knn.py
@@ -1,28 +1,86 @@
 import numpy as np
 from collections import Counter
-# # TODO: Comment all, check for model correctness
 
-def similarity(x, X):
+
+def euclidean_distance(x, X):
     xtiled = np.tile(np.reshape(x, [1, -1]), (X.shape[0], 1))
     distances = np.sum(np.square(xtiled-X), axis=1)
     return distances
 
 
-class KNNClassifier:
-    """Classifier version of the KNN. Non-parametric model that estimates the
-    label of a unseen point using its k-closests neighbors in the train set.
+class KNNBase:
+    """Base class for the KNN model.
+
+    Parameters
+    ----------
+    X : `numpy.ndarray` (n_samples, n_features)
+        Set of known points that will be used for inference.
+
+    y : `numpy.ndarray` (n_camples,)
+        Vector of labels corresponding to each point in `X`. Labels can be
+        integers indicating the class of each point in the case of the
+        classification task, or real values for regression.
+
+    k : `int`, optional
+        Number of neighbors to use to make a prediction. Defaults to 5.
+
+    distance : `str`, optional
+        Metric to use to compute the distance between points. Defaults to
+        "euclidean".
 
     """
 
-    def __init__(self, X, y, k=5):
+    def __init__(self, X, y, k=5, distance="euclidean"):
         self.X = X
         self.y = y
         self.k = k
+        self.distance = self._get_distance(distance)
+
+    def _get_distance(self, type):
+        # Return the distance metric as a callable
+        if type == "euclidean":
+            return euclidean_distance
+
+    def _get_neighbors(self, x):
+        d = self.distance(x, self.X)
+        nearest_idx = np.argsort(d)[:self.k]
+        return nearest_idx
+
+
+class KNNClassifier(KNNBase):
+    """Classifier version of the KNN.
+
+    KNNClassifier is a non-parametric model that estimates the label of a new
+    point as the majority label of its k-closests neighbors in the train set.
+    """
 
     def _predict_single(self, x):
-        d = similarity(x, self.X)
-        nearest_labels = self.y[np.argsort(d)[:self.k]]
+        nearest_idx = self._get_neighbors(x)
+        nearest_labels = self.y[nearest_idx]
         y_hat, _ = Counter(nearest_labels).most_common(1)[0]
+
+        return y_hat
+
+    def predict(self, x):
+        x = np.atleast_2d(x)
+        x = np.reshape(x, [-1, self.X.shape[1]])
+        y_hat = np.zeros(x.shape[0])
+        for i in range(len(y_hat)):
+            y_hat[i] = self._predict_single(x[i, :])
+
+        return y_hat
+
+
+class KNNRegressor(KNNBase):
+    """Regression version of the KNN.
+
+    KNNRegression is a non-parametric model that estimates the value of a new
+    point as the average its k-closests neighbors in the train set.
+    """
+
+    def _predict_single(self, x):
+        nearest_idx = self._get_neighbors(x)
+        y_hat = np.mean(self.y[nearest_idx])
 
         return y_hat
 

models/logistic_regression.py
@@ -19,6 +19,7 @@ class LogisticRegression:
     ----------
     n_features : `int`
         Dimensionality, or number of features, of the training data.
+
     """
 
     def __init__(self, n_features):
@@ -50,7 +51,6 @@ class LogisticRegression:
 
         Returns
         -------
-
         loss : `list`
             List containing the loss on the training set at each iteration
 
@@ -92,6 +92,7 @@ class LogisticRegression:
         -------
         predicted : `numpy.ndarray` (n_examples,)
             Predicted probabilities for each data example
+
         """
 
         x = np.atleast_2d(x)

models/svm.py
@@ -3,7 +3,23 @@ from utils import kernels
 
 
 class SVM:
-    # TODO: Comment all class, add radial kernel, encapsulate loss in a method
+    """SVM classifier class.
+
+    Implements a SVM classification model. The algorithm minimize the dual form
+    cost using a projected version of gradient descent.
+
+    Parameters
+    ----------
+    kernel : `str`, optional
+        Kernel to use. Defaults to "linear".
+
+    C : `float`, optional
+        Cost parameter, specifies how much we penalize incorrectly classified
+        points, the case C=inf is only possible with separable data.
+        Defaults to 1.
+
+    """
+    # TODO: Comment all class, add radial kernel
     def __init__(self, kernel='linear', C=1, degree=2):
         self.C = C
         self.kernel = self._get_kernel(kernel, degree)
@@ -30,17 +46,17 @@ class SVM:
         lagr_multipliers = np.random.rand(n_samples)/n_samples
 
         # Compute kernel_matrix labeled
-        self.G = np.zeros([n_samples, n_samples])
+        G = np.zeros([n_samples, n_samples])
         for i in range(n_samples-1):
             for j in range(i, n_samples):
-                self.G[i, j] = y[i]*y[j]*self.kernel(X[i, :], X[j, :])
-                self.G[j, i] = self.G[i, j]
+                G[i, j] = y[i]*y[j]*self.kernel(X[i, :], X[j, :])
+                G[j, i] = G[i, j]
 
         # Projected gradient descent
         loss = []
         for i in range(iterations):
-            # Gradient of hinge loss with respect to lagrange multipliers
-            dl = 0.5*np.dot(lagr_multipliers, self.G) - 1
+            # Gradient of the dual form with respect to lagrange multipliers
+            dl = np.dot(lagr_multipliers, G) - 1
 
             # Update lagrange multipliers
             lagr_multipliers -= (1/n_samples)*learning_rate*dl
@@ -49,9 +65,7 @@ class SVM:
             lagr_multipliers = np.clip(lagr_multipliers, 0, self.C)
 
             # Compute loss
-            loss.append(0.5*np.dot(np.dot(lagr_multipliers, self.G),
-                                   lagr_multipliers)
-                        - np.sum(lagr_multipliers))
+            loss.append(self.loss(lagr_multipliers, G))
 
         supported = lagr_multipliers > 1e-7
         self.lagr_multipliers = lagr_multipliers[supported]
@@ -59,18 +73,28 @@ class SVM:
         self.labels = y[supported]
 
         # Compute intercept
-        self.b = self.labels[0]
-        for i in range(1, len(self.labels)):
-            self.b -= self.lagr_multipliers[i] * self.labels[i]\
-                * self.kernel(self.supported_vectors[i],
-                              self.supported_vectors[0])
+        bias = []
+        for i in range(len(self.labels)):
+            if self.lagr_multipliers[i] < self.C:
+                b = self.labels[i]
+                for j in range(len(self.labels)):
+                    if i != j:
+                        b -= (self.lagr_multipliers[j] * self.labels[j]
+                              * self.kernel(self.supported_vectors[j],
+                                            self.supported_vectors[i]))
+                bias.append(b)
+        self.b = np.mean(np.array(bias))
 
         return loss
 
+    def loss(self, lagr_multipliers, G):
+        # Return dual form cost function
+        return 0.5*(np.dot(np.dot(lagr_multipliers, G), lagr_multipliers)
+                    - np.sum(lagr_multipliers))
+
     def _predict_single(self, x):
         K = np.array([self.kernel(xi, x) for xi in self.supported_vectors])
-        return np.clip(np.sign(np.sum(self.lagr_multipliers*self.labels*K)
-                               + self.b), 0, 1)
+        return (np.sum(self.lagr_multipliers*self.labels*K) + self.b) > 0
 
     def predict(self, x):
         x = np.atleast_2d(x)

utils/datasets.py
@@ -16,7 +16,6 @@ def blobs_classification_dataset(features, classes, samples=100):
 
     Parameters
     ----------
-
     features : `int`
         Number of dimensions of the input data.
 
@@ -26,6 +25,20 @@ def blobs_classification_dataset(features, classes, samples=100):
 
     samples : `int`, optional
         Total number of samples to generate.
+
+    Returns
+    -------
+    x_train : `numpy.ndarray` (0.8*samples, features)
+        Generated points belonging to the training set.
+
+    y_train : `numpy.ndarray` (0.8*samples,)
+        Labels corresponding to points in `x_train`.
+
+    x_test : `numpy.ndarray` (0.2*samples, features)
+        Generated points belonging to the test set.
+
+    y_train : `numpy.ndarray` (0.2*samples,)
+        Labels corresponding to points in `x_test`.
 
     """
 
@@ -47,7 +60,41 @@ def blobs_classification_dataset(features, classes, samples=100):
 
 
 def radial_classification_dataset(classes=2, samples=100, r_inc=1, noise=0.1):
-    # TODO: Comment
+    """Generates a 2D classification dataset.
+
+    Data is distributed along 0-centered circuferences. Each class falls within
+    a different radius from the center. The radius is increased with the
+    category number.
+
+    Parameters
+    ----------
+    classes : `int`, optional
+        Number of unique classes. Defaults to 2.
+
+    samples : `int`, optional
+        Total number of samples to generate.
+
+    r_inc : `float`, optional
+        Specifies the increment of radius between consecutive classes.
+
+    noise : `float`, optional
+        Standard deviation of the noise added to the samples.
+
+    Returns
+    -------
+    x_train : `numpy.ndarray` (0.8*samples, features)
+        Generated points belonging to the training set.
+
+    y_train : `numpy.ndarray` (0.8*samples,)
+        Labels corresponding to points in `x_train`.
+
+    x_test : `numpy.ndarray` (0.2*samples, features)
+        Generated points belonging to the test set.
+
+    y_train : `numpy.ndarray` (0.2*samples,)
+        Labels corresponding to points in `x_test`.
+
+    """
 
     radius = np.arange(classes)/float(r_inc)
 